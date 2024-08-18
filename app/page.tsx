10ddce4aced8468afe7f324db090416b57d61a09
Chandler Scott <cscott@aquaveo.com>
2024-08-18 16:20:14 -0600

    Link form to canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,59 @@
 'use client'
 import {RoleToken} from './ui/RoleToken'
 import { LoremIpsum } from 'lorem-ipsum';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form"
+
+interface FormValues {
+  role: string;
+  ability: string;
+  reminders: string[];
+  affectsSetup: boolean;
+  firstNight: boolean;
+  otherNights: boolean;
+}
 
 export default function Page() {
-  const randomName = Array.from(Array(Math.floor(Math.random() * 36)), () => Math.floor(Math.random() * 36).toString(36)).join('');;
-  const [roleName, setRoleName] = useState(randomName);
-  const nonsenseAbility = new LoremIpsum().generateSentences(1);
+  const {register, handleSubmit, watch, formState: { errors },} = useForm<FormValues>({
+    defaultValues: { 
+      role: Array.from(Array(Math.floor(Math.random() * 36)), () => Math.floor(Math.random() * 36).toString(36)).join(''), 
+      ability: new LoremIpsum().generateSentences(1),
+      reminders: [],
+      affectsSetup: false,
+      firstNight: false,
+      otherNights: false
+    }
+  });
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
+
   return (
     <div>
       <RoleToken
         diameter={256} 
-        reminderCount={7}
-        affectsSetup={true}
-        firstNight={true}
-        otherNights={true}
-        name={roleName}
-        ability={nonsenseAbility}
+        reminderCount={watch('reminders')?.length || 0}
+        affectsSetup={watch('affectsSetup')}
+        firstNight={watch('firstNight')}
+        otherNights={watch('otherNights')}
+        name={watch('role')}
+        ability={watch('ability')}
       />
-    <RoleToken
-      diameter={512} 
-      reminderCount={7}
-      affectsSetup={true}
-      firstNight={true}
-      otherNights={true}
-      name={roleName}
-      ability={nonsenseAbility}
-    />
-    <RoleToken
-      diameter={768} 
-      reminderCount={7}
-      affectsSetup={true}
-      firstNight={true}
-      otherNights={true}
-      name={roleName}
-      ability={nonsenseAbility}
-    />
-    <br />
-    <form>
-      <input type="text" value={roleName} onChange={(e) => setRoleName(e.target.value)} />
-    </form>
-  </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="role">Role</label>
+        <input {...register("role")} />
+        <label htmlFor="ability">Ability</label>
+        <input {...register("ability")} />
+        <label htmlFor="reminders">Reminders</label>
+        <input {...register("reminders")} />
+        <label htmlFor="affectsSetup">Affects Setup</label>
+        <input type="checkbox" {...register("affectsSetup")} />
+        <label htmlFor="firstNight">First Night</label>
+        <input type="checkbox" {...register("firstNight")} />
+        <label htmlFor="otherNights">Other Nights</label>
+        <input type="checkbox" {...register("otherNights")} />
+        <button type="submit">
+          Save
+        </button>
+      </form>
+    </div>
   )
 }
